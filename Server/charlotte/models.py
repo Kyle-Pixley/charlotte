d70f992aafc4c777cd91635206718d9337802e70
Kyle-Pixley <kyle_pixley@hotmail.com>
2026-06-04 14:09:28 -0400

    message server model

--- Server/charlotte/models.py
+++ Server/charlotte/models.py
@@ -13,3 +13,44 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    participants = models.ManyToManyField(User, related_name="chatrooms")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.name:
+            return self.name
+        return f"ChatRoom {self.id}"
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        user,
+        on_delete=models.CASCADE,
+        related_name="sent_messages"
+    )
+    #For direct messages
+    receiver = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="received_messages",
+        blank=True,
+        null=True
+    )
+    #For room messages
+    chatroom = models.ForeignKey(
+        ChatRoom,
+        on_delete=models.CASCADE,
+        related_name="messages",
+        blank=True,
+        null=True
+    )
+
+    is_read = models.BooleanField(default=False)
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.chatroom:
+            return f"From {self.sender} in {self.chatroom} at {self.timestamp}"
+        return f"From {self.sender} to {self.receiver} at {self.timestamp}"
