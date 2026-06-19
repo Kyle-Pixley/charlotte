c683eb8623e4fe338335dcd18e2cf2dc85592eb6
Kyle-Pixley <kyle_pixley@hotmail.com>
2026-06-19 00:04:25 -0400

    urls

--- Server/charlotte/urls.py
+++ Server/charlotte/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('login_user/', views.login_user),
     path('find_user_id/', views.find_user_id),
     path('find_username/', views.find_username),
+    path('create_chatroom/', views.create_chatroom),
+    path('request_to_join_chatroom/', views.request_to_join_chatroom),
+    path('approve_join_request/', views.approve_join_request),
     
 ]
